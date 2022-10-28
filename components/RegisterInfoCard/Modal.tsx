1ab9f679386a4c218fbe5c0275568cf344874302
abuzarmushtaq <abuzarmushtaq2016@outlook.com>
2022-10-28 07:30:07 +0500

    Add: Log In Modal

---
+++ components/RegisterInfoCard/Modal.tsx
@@ -0,0 +1,34 @@
+import { useRouter } from 'next/router'
+import { useRecoilState } from 'recoil'
+import { registerIndexState } from 'state/auth'
+import { YouTube } from 'state/auth'
+import styles from './styles.module.css'
+
+interface Props {
+  open: boolean
+  index: number
+  email: string
+  youtubeInfo: YouTube
+  onClose?: Function
+}
+
+export const Modal = ({ open, index, email, youtubeInfo, onClose }: Props) => {
+  const router = useRouter()
+  const [, setRegisterIndex] = useRecoilState(registerIndexState)
+  const onClick = () => {
+    setRegisterIndex(index)
+    router.push('/')
+  }
+  return open ? (
+    <div className={styles.modal} onClick={() => onClose && onClose()}>
+      <div className={styles.modalContent}>
+        <p>Please enter the required information.</p>
+        <div>
+          <button onClick={onClick}>Log In</button>
+        </div>
+      </div>
+    </div>
+  ) : (
+    <></>
+  )
+}
